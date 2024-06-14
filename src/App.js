f7842b0af14a71a423404757fcbd90a6ff46bea5
Tushar <tusharkaler450@gmail.com>
2024-06-14 20:06:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from "axios";
 import './App.css';
+import Loader from './Loader/Loader'
 
 function App() {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState(["1.................................", "2", "3"]);
+  const [loading, setLoading] = useState(false);
+  const handleClick = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post("http://localhost:5000/api/GenAIGenerate/generate", value);
+      setResult(res);
+    } catch (error) {
+      setResult(["Something went wrong...."]);
+      console.log(error);
+      throw error;
+    }
+    finally {
+      setLoading(false);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+        <div className='above'>
+          {loading ? <Loader /> :
+            result.length > 0 &&
+            result.map((it) => {
+              return <div>{it}</div>
+            })}
+        </div>
+        <div className='bottom'>
+          <input value={value} onChange={(e) => {
+            setValue(e.target.value);
+          }} />
+          <button onClick={handleClick} disabled={loading} title='Search'>Search</button>
+        </div>
+      </div>
     </div>
   );
 }
